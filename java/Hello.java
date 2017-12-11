fb468283de29c263743e79f30f8d1606166d4f51
kmjbebe0701 <kmj_0701@naver.com>
2017-12-11 15:58:57 +0900

    java Hello프로그램 작성

---
+++ java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	// main method(메서드), 프로그램의 시작점
+	//String[] args: argument, parameter
+	public static void main(String[] args){
+		
+		//System.out.print 메서드: 괄호 안의 글자를 출력
+		System.out.println("Hello, World");
+	}
+}	
