4491655aaef20fb093a84b74fe5a5376b9c0b316
kmjbebe0701 <kmj_0701@naver.com>
2017-12-14 16:46:34 +0900

    강아지 비교하기 응용문제07

--- problem07/TestPuppy.java
+++ problem07/TestPuppy.java
@@ -6,10 +6,15 @@ public class TestPuppy {
 			new Puppy("진돗개", "에이", 3, "남");
 		Puppy B = 
 			new Puppy("삽살개", "비", 2, "여");
+		Puppy C = 
+			new Puppy("진돗개", "에이", 2, "여");	
 			
 			
 		boolean isEqual = A.equals(B);
 		System.out.println(isEqual);
+		
+		boolean isEqual02 = A.equals(C);
+		System.out.println(isEqual02);
 			
 		
 	}
