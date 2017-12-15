0bda377916894bf966fa994f4e128a510ab46b10
kmjbebe0701 <kmj_0701@naver.com>
2017-12-15 15:05:35 +0900

    두 수를 입력받아 계산하기

--- problem08/TestCalc.java
+++ problem08/TestCalc.java
@@ -5,18 +5,20 @@ public class TestCalc {
 		Calc a = new Calc ();
 	
 		
+		int q = Integer.parseInt(args[0]);
+		int p = Integer.parseInt(args[1]);
 	
 	
-	int result1 = a.add(5, 7);
+	int result1 = a.add(q, p);
 	System.out.println("두 수를 더하면" + result1);
 	
-	int result2 = a.sub(2, 1);
+	int result2 = a.sub(q, p);
 	System.out.println("두 수를 빼면" +result2);
 	
-	int result3 = a.mul(3, 2);
+	int result3 = a.mul(q, p);
 	System.out.println("두 수를 곱하면" +result3);
 	
-	int result4 = a.div(4, 2);
+	int result4 = a.div(q, p);
 	System.out.println("두 수를 나누면" +result4);
 	
 	}
